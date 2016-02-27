0397bb640f8395701a107a2089bab8179a9940fa
Jake Barnwell <jb16@mit.edu>
2016-02-27 18:54:38 -0500

    Add enter event for text box submission

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@
 $(function () {
     loadMapTransition(7, 1);
     $('#change_button').click(loadMapEvent);
+    $("#floor_input").keypress(function(e) {
+		if(e.which === 13) {
+ 			e.preventDefault();
+ 			$("#change_button").click();
+ 		}
+    });
     $("#up-button").click(function() {
     	loadMapTransition(currentBldg, currentFloor+1);
     });
