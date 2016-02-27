2577b5396cfcf59ee668f20781f357f07130bdb8
Jake Barnwell <jb16@mit.edu>
2016-02-27 16:32:42 -0500

    stilld iong logic to populate notes info for a node" a;sildg

--- js/events.js
+++ js/events.js
@@ -1,8 +1,24 @@
+var a = 0;
 
+function get_amenity_object(bldg, floor, type, id) {
+	possibles = resource_data[bldg][floor][type];
+	for(var i = 0; i < possibles.length; i++) {
+		if(possibles[i]["id"] === id) {
+			return possibles[i];
+		}
+	}
+	return null;
+}
 function add_resource_handlers() {
-    $(".resource").click(function(e) {
-        console.log("clicked!");
-        $("#icon-notes-title").text("Notes for " + 3);
+    $(".resource-img").click(function(e) {
+    	var target = $(e.target);
+        $("#icon-notes-title").html("Notes for this <b>" + target.attr("data-type") + "</b>:");
+        // var notes = get_amenity_object(targ, floor, type, id);
+        var notes = "";
+        if(notes === null || notes === "") {
+        	notes = "No available notes.";
+        }
+        $("#icon-notes-body").html(notes);
         $("#icon-notes").modal('show');
     });
 }
