0f12fdd7b351675cb794cf213dcfa35ac841494f
Jake Barnwell <jb16@mit.edu>
2016-02-27 18:20:01 -0500

    Add up/down floors button

--- js/script.js
+++ js/script.js
@@ -8,4 +8,10 @@
 $(function () {
     loadMapTransition(7, 1);
     $('#change_button').click(loadMapEvent);
+    $("#up-button").click(function() {
+    	loadMapTransition(currentBldg, currentFloor+1);
+    });
+    $("#down-button").click(function() {
+    	loadMapTransition(currentBldg, currentFloor-1);
+    });
 });
