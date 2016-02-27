d8ffe0a5d84f19fb84b6da23a426efe79e756516
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2016-02-27 10:20:23 -0500

    change floor code for dev

--- js/util.js
+++ js/util.js
@@ -6,15 +6,11 @@ function calcPostion (event) {
     var map = $('#map');
     var map_width = map.css('width');
     var map_height = map.css('height');
+    var map_offset = map.offset();
     map_width = Number(map_width.substring(0, map_width.length-2));
     map_height = Number(map_height.substring(0, map_height.length-2));
-    map_offset = map.offset();
-    map_x = (pageX - map_offset.left) / map_width;
-    map_y = (pageY - map_offset.top) / map_height;
-
-    console.log(pageX, pageY);
-    console.log(map_offset.left, map_offset.top);
-    console.log(map_x, map_y);
+    var map_x = (pageX - map_offset.left) / map_width;
+    var map_y = (pageY - map_offset.top) / map_height;
 
     return {
         'x': map_x,
@@ -30,10 +26,14 @@ function displayPostion (event) {
 }
 
 function changeMap(event) {
-    
+    var map_id = $('#floor_input').val();
+    var src = 'resources/img/' + map_id + '.svg';
+
+    $('#map').attr('src', src);
 }
 
 
 $(function () {
     $('#map').click(displayPostion);
+    $('#change_button').click(changeMap);
 });
